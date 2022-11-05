825ca0be9697fe07413ec5d33a3afd8e2da4ab15
Zalim Tsorionov <zalim.tsorion@gmail.com>
2022-11-05 12:59:12 +0200

    05112022

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,19 @@
 <template>
 <div class="form-wrapper">
     <div class="container">
-    <form ref="form" @submit.prevent="sendEmail" id="contact" class="form">
-        <input name="name" required type="text" placeholder="Name" v-model="name">
-        <input name="email" required type="email" placeholder="Email" v-model="email">
-        <textarea name="message" required id="message" rows="5" placeholder="Message" v-model="message"></textarea>
-        <button type="submit">SEND</button>
-    </form>
+        <form ref="form" @submit.prevent="sendEmail" id="contact" class="form">
+            <input name="name" required type="text" placeholder="Name" v-model="name">
+            <input name="email" required type="email" placeholder="Email" v-model="email">
+            <textarea name="message" required id="message" rows="5" placeholder="Message" v-model="message"></textarea>
+            <button type="submit">SEND</button>
+        </form>
+        <div v-show="isActive" class="message-success-modal">
+            <div class="message-success">
+                <div @click="isActive=false" class="close-modal">✕</div>
+                <p>Your Message Successfully Sent</p>
+                <p><strong>Thank you!</strong></p>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -22,28 +29,45 @@ export default {
         return {
             name: "",
             email: "",
-            message: ""
+            message: "",
+
+            isActive: false,
         }
     },
     methods: {
-        handleSubmit() {
-            let userMessage = {
-                name: this.name,
-                email: this.email,
-                message: this.message,
-                date: new Date()
-            }
+        // handleSubmit() {
+        //     let userMessage = {
+        //         name: this.name,
+        //         email: this.email,
+        //         message: this.message,
+        //         date: new Date()
+        //     }
 
-            console.log(userMessage)
-            projectFirestore.collection('Incoming Messages').add(userMessage);
-        },
+        //     console.log(userMessage)
+        //     projectFirestore.collection('Incoming Messages').add(userMessage);
+        // },
         sendEmail() {
             emailjs.sendForm('zalim_gmail', 'contact_form', this.$refs.form, 'oOpGZlSuR-6lhWtcy')
                 .then((result) => {
                     console.log('SUCCESS!', result.text);
+                    this.isActive = true;
                 }, (error) => {
                     console.log('FAILED...', error.text);
                 });
+
+                let userMessage = {
+                    name: this.name,
+                    email: this.email,
+                    message: this.message,
+                    date: new Date()
+                }
+
+                // console.log(userMessage);
+                projectFirestore.collection('Incoming Messages').add(userMessage);
+
+                this.name = "";
+                this.email = "";
+                this.message = "";
             }
     }
 }
@@ -54,6 +78,7 @@ export default {
 
     .form-wrapper {
         background: #151515;
+        padding-bottom: 50px;
 
         form {
             display: flex;
@@ -99,8 +124,40 @@ export default {
                 font-size: 18px;
                 font-weight: bold;
                 cursor: pointer;
+                border: none;
+              }
             }
-        }
+
+            .message-success-modal {
+                background: rgba(0,0,0,0.6);
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100vw;
+                height: 100vh;
+                // display: none;
+
+                .close-modal {
+                    color: #fff;
+                    position: absolute;
+                    top: 7px;
+                    right: 15px;
+                    font-size: 30px;
+                    cursor: pointer;
+                }
+
+                .message-success {
+                    background: #000;
+                    color: $yellow;
+                    max-width: 300px;
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    padding: 40px;
+                }
+            }
+
     }
     
 </style>
